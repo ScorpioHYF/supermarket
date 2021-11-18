e34323c46054258d79a7ec528ce9e8b5ab18efa0
Alex <987057983@qq.com>
2021-11-18 15:08:09 +0800

    as

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -1,55 +1,60 @@
 <template>
-	<div class="tab-control">
-		<div class="tab-ctrl-item" v-for="(item, index) in titles" :key="index"
-			:class="{active: index === currentIndex}" @click="itemClick(index)">
-			<span>{{item}}</span>
-		</div>
-	</div>
+  <div class="tab-control">
+    <div
+      class="tab-ctrl-item"
+      v-for="(item, index) in titles"
+      :key="index"
+      :class="{ active: index === currentIndex }"
+      @click="itemClick(index)"
+    >
+      <span>{{ item }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'TabControl',
-		props: {
-			titles: {
-				type: Array,
-				default () {
-					return []
-				}
-			}
-		},
-		data() {
-			return {
-				currentIndex: 0
-			}
-		},
-		methods: {
-			itemClick(index) {
-				this.currentIndex = index
-			}
-		}
-	}
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
 </script>
 
 <style scoped lang="less">
-	.tab-control {
-		display: flex;
-		margin-top: 10px;
-		height: 40px;
-		line-height: 40px;
-		text-align: center;
+.tab-control {
+  display: flex;
+  margin-top: 10px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
 
-		.tab-ctrl-item {
-			flex: 1;
-		}
+  .tab-ctrl-item {
+    flex: 1;
+  }
 
-		.active {
-			color: var(--color-high-text);
+  .active {
+    color: var(--color-high-text);
 
-			span {
-				border-bottom: 3px solid var(--color-tint);
-				padding: 5px;
-			}
-		}
-	}
+    span {
+      border-bottom: 3px solid var(--color-tint);
+      padding: 5px;
+    }
+  }
+}
 </style>
